cfa5452a786ad9227e4a1763092d40b1a8c84681
Pitchayut Ariyachansil <00479@kvis.ac.th>
2021-12-01 00:10:19 +0700

    first deploy

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Subject(BaseModel):
+    subject: str
+    title: str
+    author: str
+    des: str
+    url: str
+
+subject_db = [
+    {
+        'subject':'Math',
+        'title': 'Math01',
+        'author': 'Idiot',
+        'des': 'Yeah',
+        'url': 'https://youtu.be/1OO5LnnGPtg',
+    },
+    {
+        'subject': 'Introduction',
+        'title': 'PR staff',
+        'author': 'Yeah?',
+        'des': 'hEllo Yo man',
+        'url': 'https://youtu.be/1OO5LnnGPtg',
+    },
+]
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return subject_db
